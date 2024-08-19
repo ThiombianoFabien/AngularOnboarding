09d5adc69186d5554c6ed4b799dd3c2b7e95c9eb
Fabien Thiombiano <fabienthiombiano@gmail.com>
2024-08-19 01:06:22 +0000

    sportstore - project init

---
+++ SportsStore/src/app/model/product.repository.ts
@@ -0,0 +1,20 @@
+import { computed, Injectable, Signal } from '@angular/core';
+import { Product } from './product.model';
+import { StaticDataSource } from './static.dataSource';
+
+@Injectable()
+export class ProductRepository {
+  products: Signal<Product[]>;
+  categories: Signal<string[]>;
+
+  constructor(private dataSource: StaticDataSource) {
+    this.products = dataSource.products;
+    this.categories = computed(() => {
+      return this.dataSource
+        .products()
+        .map((p) => p.category ?? 'None')
+        .filter((c, index, array) => array.indexOf(c) == index)
+        .sort();
+    });
+  }
+}
